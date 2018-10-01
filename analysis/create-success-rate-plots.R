860ed97ba43b2310b849f05e0f95663dbc315228
Pedro Lopes da Silva Mariano <plmariano@ciencias.ulisboa.pt>
2018-10-01 17:03:49 +0100

    Added options to have multiple random chance to win and to compute automatic experiment parameters.

--- analysis/create-success-rate-plots.R
+++ analysis/create-success-rate-plots.R
@@ -2,7 +2,9 @@ library ("data.table")
 library ("ggplot2")
 
 create.success.rate.boxplots <- function (
-  experiment.filename
+  experiment.filename,
+  automatic.experiment.parameters = TRUE,
+  single.random.chance.wind = FALSE
 ) {
   experiments <- fread (experiment.filename)
   data <- experiments [
@@ -29,8 +31,17 @@ create.success.rate.boxplots <- function (
       ylab = NA,
       xlab = "score",
       horizontal = TRUE,
-      las = 1
+      las = 1,
+      yaxt = ifelse (automatic.experiment.parameters, "s", "n")
     )
+    if (!automatic.experiment.parameters) {
+      axis (
+        side = 2,
+        at = seq (1, nrow (experiments)),
+        labels = experiments [, labels],
+        las = 1
+      )
+    }
     if (show.iterations.performed) {
       Ns <- data [
         ,
@@ -47,10 +58,30 @@ create.success.rate.boxplots <- function (
         las = 1
       )
     }
-    abline (
-      v = data [, mean (random.chance.win)],
-      lty = 3
-    )
+    if (single.random.chance.wind) {
+      abline (
+        v = data [, mean (random.chance.win)],
+        lty = 3
+      )
+    } else {
+      ys <- data [
+        ,
+        mean (random.chance.win),
+        by = .(
+            data.sets,
+            learning.parameters,
+            fraction.test
+            )
+        ]
+      ys <- ys [, V1]
+      points (
+        x = ys,
+        y = seq (1, nrow (experiments)),
+        col = "#FF0000",
+        pch = 4,
+        lwd = 2
+      )
+    }
   }
   png (
     filename = "performance.png",
@@ -113,12 +144,12 @@ create.success.rate.image.plot.for.2.classes <- function (
     ) + scale_colour_gradient (
       low = "#FF0000",
       high = "#00FF00",
-      labels = function (v) return (sprintf ("%d%%", v * 100))
+      labels = function (v) return (sprintf ("%.0f%%", v * 100))
     ) + scale_size (
-      labels = function (v) return (sprintf ("%d%%", v * 100))
+      labels = function (v) return (sprintf ("%.0f%%", v * 100))
     ) + theme_bw (
     ) + labs (
-      title = "median of classifier success rate in a data set with two classes",
+      title = "median of classifier success rate\nin a data set with two classes",
       x = "class A",
       y = "class B",
       colour = "success rate",
